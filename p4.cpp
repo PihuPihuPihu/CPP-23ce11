46baf5bfe62f1f849eef7dd2263d41438088cd9e
PihuPihuPihu <155663268+PihuPihuPihu@users.noreply.github.com>
2024-10-23 11:04:38 +0530

    Update p4.cpp

--- p4.cpp
+++ p4.cpp
@@ -2,4 +2,10 @@
 using namespace std;
 Class Prod{
   public:
-  Prod(int &a, int &b){
+  Prod(int a, int b){
+         cout<<a+b;}
+  Prod(float a,float b){
+         cout<<a+b;}
+  Prod(char a,char b){
+         cout<<a+b;}
+  
